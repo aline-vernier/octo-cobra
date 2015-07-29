540ce61d74b2ae30da7768e9f125cb25eae5a586
Aline Vernier <aline.vernier@ensta.fr>
2015-07-29 11:47:55 +0200

    Added RMS value to script

--- DM Analysis/Wavefronts/Wavefronts.py
+++ DM Analysis/Wavefronts/Wavefronts.py
@@ -4,22 +4,55 @@ Created on Fri Jul 24 18:16:54 2015
 
 @author: aline
 """
-import re
-from re import * 
+
 import numpy as np
-import scipy.sparse as ssp
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import parseWF
+from parseWF import *
 
-myDir = '/home/aline/programming/Python/DM Analysis/Wavefronts/20150710/'
+myDir =  os.getcwd()+'/20150710_raw/'
 
+#Parse and covert files to something sensible if they're not already converted
+for file in os.listdir(myDir):
+    if (file.startswith("WF") and not file.endswith("parsed.txt")): 
+        
+        parse_WF(myDir + file);
+        
+#Load parsed files and save to pictures (.tiff)
 
-with open(myDir + 'WF_Zonal_PleineEnergie6.txt') as f:
-    pupil = f.readlines()[9:] 
     
+ 
+for file in os.listdir(myDir):
+    pupil = []   
+    RMS = 0.
+    num = 0.
+    if file.endswith("parsed.txt"): 
+        with open(myDir + file, 'r') as f: 
+            for line in f.readlines():
+                pupil.append(
+                map(float, line.split()))
+            #
+            pupil = np.array(pupil)
+            flatPupil = pupil.flatten()
+            for element in flatPupil:
+                if not np.isnan(element):
+                    RMS = RMS + element*element
+                    num = num + 1
 
-for i in range(0,len(pupil)):
-    pupil[i] = sub(',', '.', sub('\t\t', '\t0.\t', pupil[i]))
-    pupil[i] = np.array(map(float, pupil[i].split()))
-    print(len(pupil[i]))
-pupil = np.array(pupil)
+            RMS = np.around(np.sqrt(RMS/num), decimals = 3)
+                
+            fig, ax = plt.subplots()
+            ax.text(25,3, 'RMS = ' + str(1000*RMS) + 'nm', fontsize = 11)
 
-#pupil = np.lib.pad(pupil, max(len(l) for l in pupil), 'minimum')
+            cax = ax.imshow(pupil, interpolation = 'none', cmap = cm.afmhot )
+            
+            cbar = fig.colorbar(cax)
+            
+            #plt.show()
+            plt.savefig(os.path.splitext(file)[0] + '_RMS.tiff' )
+            plt.cla()
+       #file_out.close()
+
+
+
